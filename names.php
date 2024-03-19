7757351b637624aeabaea0a3de6ebece490f4909
Angel203 <angelb203@icloud.com>
2024-03-19 11:58:37 -0400

    added more code

--- names.php
+++ names.php
@@ -1,13 +1,35 @@
 <?php
     
-    function greet(){
-        $names = array("Mariah", "Robert", "Ezekiel", "Kailin");
-        foreach ( $names as $currentName ) {
-            echo $currentName . "<br>";
+    function filterNames($names){
+        $filteredNames = array();
+
+        foreach ($names as $name){
+            if (strlen($name)<= 7){
+                $filteredNames[]= $name;
+            }
         }
-        // if ($names)
-        // echo("Hi how are you\n");
+        return $filteredNames;
     }
 
-    greet();
+    $names = array("Mariah", "RJ", "Ezekiel", "Kailina", "Zeek", "Lina");
+
+    $filteredNames = filterNames($names);
+
+    echo "Names with 7 or fewer letters: \n";
+    foreach ($filteredNames as $name){
+    
+        echo $name . "\n";
+    }
+
+
+    // function greet(){
+    //     $names = array("Mariah", "Robert", "Ezekiel", "Kailin");
+    //     foreach ( $names as $currentName ) {
+    //         echo $currentName . "<br>";
+    //     }
+    //     // if ($names)
+    //     // echo("Hi how are you\n");
+    // }
+
+    // greet();
 ?>
